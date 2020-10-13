1275b6727a0b3bafef410474a91e3cdff728dc9c
Kemosabe2911 <stevinprince29@gmail.com>
2020-10-13 16:59:29 +0530

    Error validation script

--- server.js
+++ server.js
@@ -20,8 +20,24 @@ app.get('/home',(req,res) =>{
 });
 
 app.post('/register',(req,res)=>{
-    
-})
+
+    let {name, email, password, password2} = req.body;
+    console.log({name,email,password,password2});
+    //Error validation
+    let errors= [];
+
+    if(!name || !email || !password || !password2){
+        errors.push({message: "Please enter all fields"});
+    }
+
+    if(password.length<6){
+        errors.push({message: "Password should be atleast 6 charachters"});
+    }
+
+    if(password != password2){
+        errors.push({message: "Passwords do not match"});
+    }
+});
 
 app.listen(PORT, () =>{
     console.log(`Server Running on port ${PORT}`);
