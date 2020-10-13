7f12e7781266d5e6a8f919192d5565a866733417
Kemosabe2911 <stevinprince29@gmail.com>
2020-10-13 16:50:03 +0530

    Add middleware

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const { pool }= require("./dbConfig");
 const PORT = process.env.PORT || 5000;
 
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({extended: false}));
 
 app.get('/',(req,res) =>{
     res.render("login");
@@ -17,6 +18,10 @@ app.get('/register',(req,res) =>{
 app.get('/home',(req,res) =>{
     res.render("index",{user: "Stevin"});
 });
+
+app.post('/register',(req,res)=>{
+    
+})
 
 app.listen(PORT, () =>{
     console.log(`Server Running on port ${PORT}`);
