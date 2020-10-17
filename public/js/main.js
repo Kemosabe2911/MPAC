614b718f5651df3164fe0b675e99d2d55019277f
Kemosabe2911 <stevinprince29@gmail.com>
2020-10-17 11:36:55 +0530

    Add main.js

---
+++ public/js/main.js
@@ -0,0 +1,12 @@
+
+//Home Page 
+
+//Buy
+document.getElementById('buy').addEventListener('click',()  =>{
+    window.location.href="index.html";
+});
+
+//Sell
+document.getElementById('sell').addEventListener('click',()  =>{
+    window.location.href="index.html";
+});
