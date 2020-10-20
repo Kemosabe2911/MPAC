70c0bd0563db4ac20f31441424c88765afa5eed5
Kemosabe2911 <stevinprince29@gmail.com>
2020-10-20 19:53:46 +0530

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -3,10 +3,10 @@
 
 //Buy
 document.getElementById('buy').addEventListener('click',()  =>{
-    window.location.href="index.html";
+    window.location.href="/buy";
 });
 
 //Sell
 document.getElementById('sell').addEventListener('click',()  =>{
-    window.location.href="index.html";
+    window.location.href="/sell";
 });
