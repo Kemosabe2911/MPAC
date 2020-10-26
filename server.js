089990097b37f5e59109bb439ec6ad2653257566
Kemosabe2911 <stevinprince29@gmail.com>
2020-10-26 11:46:17 +0530

    Update server

--- server.js
+++ server.js
@@ -1457,6 +1457,36 @@ app.post('/purchase',(req,res) =>{
         <li>Price: ${req.body.price}</li>
     </ul>
     `;
+
+    let transporter = nodemailer.createTransport({
+        host: 'smtp.googlemail.com',
+        port: 465,
+        secure: true, // true for 465, false for other ports
+        auth: {
+        user: 'itsmestevin29@gmail.com', // generated ethereal user
+        pass: 'stevin@2911', // generated ethereal password
+        },
+        tls:{
+            rejectUnauthorized: false
+        }
+    });
+
+    // send mail with defined transport object
+    let info = transporter.sendMail({
+        from: '"Template Email" <itsmestevin29@gmail.com>', // sender address
+        to: `${req.user.email}`, // list of receivers
+        subject: "MPAC Purchase Confirmation", // Subject line
+        text: "Hello world?", // plain text body
+        html: output, // html body
+    });
+
+    console.log("Message sent: %s", info.messageId);
+    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+    // Preview only available when sending through an Ethereal account
+    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+
+    res.render("buy-y1-books");
 });
 
 
