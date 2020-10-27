f2a0e0b1a7d630961a879a53e4fc9dde586d64e9
Kemosabe2911 <stevinprince29@gmail.com>
2020-10-27 15:10:31 +0530

    Server updated

--- server.js
+++ server.js
@@ -1481,7 +1481,7 @@ app.post('/purchase-books',(req,res) =>{
     </ul>
     `;
     let transporter = nodemailer.createTransport({
-        host: 'smtp.googlemail.com',
+        host: 'smtp.gmail.com',
         port: 465,
         secure: true, // true for 465, false for other ports
         auth: {
@@ -1513,7 +1513,7 @@ app.post('/purchase-books',(req,res) =>{
     // Preview only available when sending through an Ethereal account
     console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
 
-    res.render("purchase-books");
+    res.redirect("/home");
 
     });
 });
