a9b4ccf3ba5f5fb1e3030422451ac11fb249208c
Kemosabe2911 <stevinprince29@gmail.com>
2020-10-29 12:27:48 +0530

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -20,3 +20,16 @@ const fileChosen = document.getElementById('file-chosen');
 pImage.addEventListener('change', function(){
   fileChosen.textContent = this.files[0].name
 })
+
+
+//Dashboard
+
+//My Products
+document.getElementById('my-prod').addEventListener('click',()  =>{
+    window.location.href="/my-prod";
+});
+
+//My Cart
+document.getElementById('my-cart').addEventListener('click',()  =>{
+    window.location.href="/my-cart";
+});
