8b979a34ad50e00411ad3c525c3bd4cfebb6ea6e
Kemosabe2911 <stevinprince29@gmail.com>
2020-11-10 11:35:28 +0530

    Update home

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,17 @@
 
 //Home Page 
+var myNav = document.getElementById('navbar');
+window.onscroll = function () { 
+    "use strict";
+    if (document.body.scrollTop >= 100 || document.documentElement.scrollTop >= 100 ){
+        myNav.classList.add("nav-colored");
+        myNav.classList.remove("nav-transparent");
+    } 
+    else {
+        myNav.classList.add("nav-transparent");
+        myNav.classList.remove("nav-colored");
+    }
+};
 
 //Buy
 document.getElementById('buy').addEventListener('click',()  =>{
